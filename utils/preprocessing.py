d0483027318117774a97892ba588f7b341e82c43
khaliesahazmin <khaliesah169@gmail.com>
2025-05-06 14:05:35 +0800

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -5,8 +5,24 @@ from PIL import Image
 def preprocess_image(image):
     image = np.array(image)
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    _, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+
+    # Deskew
+    coords = np.column_stack(np.where(gray > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = gray.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    gray = cv2.warpAffine(gray, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+    # Thresholding
+    binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                   cv2.THRESH_BINARY, 11, 2)
+
+    # Denoising
     denoised = cv2.medianBlur(binary, 3)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-    sharpened = cv2.morphologyEx(denoised, cv2.MORPH_GRADIENT, kernel)
-    return Image.fromarray(sharpened)
+
+    return Image.fromarray(denoised)
